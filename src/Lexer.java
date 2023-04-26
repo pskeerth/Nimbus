99b7d6c0db26b37b380841d5bdb684f1d7875600
shanmugapriyan98 <savic23@asu.edu>
2023-04-26 15:37:51 -0700

    Corrected blank files and commited them

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Lexer {
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Lexer {
+    public static void main(String[] args) throws IOException {
+
+        String filename = "";
+        filename = args.length > 0 ? args[0] : null;
+        if (filename.equals(null)){
+            System.out.println("input filename can't be empty");
+            return;
+        }
+
+        if (!filename.contains(".nimb")) {
+            System.out.println("Enter a valid .nimb file");
+            return;
+        }
+
+        CharStream input = CharStreams.fromFileName(filename);
+        NimbusLexer lexer = new NimbusLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        tokens.fill();
+        FileWriter writer = new FileWriter(filename+"tokens");
+        for (Token token : tokens.getTokens()) {
+            System.out.print("hi");
+            System.out.println(token);
+            writer.write(token.getText() + "\n");
+        }
+        writer.close();
+    }
 }
