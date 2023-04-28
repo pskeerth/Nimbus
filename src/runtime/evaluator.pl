32b4a50a6c2bb2d65fce2a468add1b6b5dd66fce
Joseph Thomas <jthom175@asu.edu>
2023-04-28 16:45:34 -0700

    Modified Semantics

--- src/runtime/evaluator.pl
+++ src/runtime/evaluator.pl
@@ -1,3 +1,44 @@
+program_eval(t_program(K), NewEnv) :- block_eval(K, [], NewEnv).
+block_eval(t_block(D, C), Env, NewEnv) :- declblock_eval(D, Env, Env1), command_eval(C, Env1, NewEnv).
+
+declblock_eval(t_decl_block(D), Env, NewEnv) :- decl_eval(D, Env, NewEnv).
+declblock_eval(t_decl_block(D, R), Env, NewEnv) :- decl_eval(D, Env, Env1), declblock_eval(R, Env1, NewEnv).
+
+generalblock_eval(t_gen_block(Cmd_block), Env, NewEnv) :- command_eval(Cmd_block, Env, NewEnv).
+
+decl_eval(t_decl('int', ID, Val), Env, NewEnv) :- update('int', ID, Val, Env, NewEnv).
+decl_eval(t_decl('float', ID, Val), Env, NewEnv) :- update('float', ID, Val, Env, NewEnv).
+decl_eval(t_decl('string', ID, Val), Env, NewEnv) :- update('string', ID, Val, Env, NewEnv).
+decl_eval(t_decl('boolean', ID, Val), Env, NewEnv) :- update('boolean', ID, Val, Env, NewEnv).
+decl_eval(t_decl('int', ID), Env, NewEnv) :- update('int', ID, 0, Env, NewEnv).
+decl_eval(t_decl('float', ID), Env, NewEnv) :-  update('float', ID, 0.0, Env, NewEnv).
+
+command_eval(t_command(Cmd_block), Env, NewEnv) :- commandblock_eval(Cmd_block, Env, NewEnv).
+command_eval(t_command(Cmd_block, Cmd), Env, NewEnv) :- commandblock_eval(Cmd_block, Env, Env1), command_eval(Cmd, Env1, NewEnv).
+
+if_statement_eval(t_if_statement(Bool_expr, Gen_block), Env, NewEnv) :- boolean_eval(Bool_expr, Env, Env1, true), generalblock_eval(Gen_block, Env1, NewEnv).
+if_statement_eval(t_if_statement(Bool_expr, _), Env, NewEnv) :- boolean_eval(Bool_expr, Env, NewEnv, false).
+
+if_else_statement_eval(t_if_else_statement(Bool_expr, Gen_block1, _), Env, NewEnv) :- boolean_eval(Bool_expr, Env, Env1, true), generalblock_eval(Gen_block1, Env1, NewEnv).
+if_else_statement_eval(t_if_else_statement(Bool_expr, _, Gen_block2), Env, NewEnv) :- boolean_eval(Bool_expr, Env, Env1, false), generalblock_eval(Gen_block2, Env1, NewEnv).
+
+while_statement_eval(t_while_statement(Bool_expr, Gen_block), Env, NewEnv) :- boolean_eval(Bool_expr, Env, Env1, true), generalblock_eval(Gen_block, Env1, Env2),
+    																		  while_statement_eval(t_while_statement(Bool_expr, Gen_block), Env2, NewEnv).
+while_statement_eval(t_while_statement(Bool_expr, _), Env, NewEnv) :- boolean_eval(Bool_expr, Env, NewEnv, false).
+
+for_loop_eval(t_for_loop(Identifier, Integer_val, Bool_expr, Unary_expr, Gen_block), Env, NewEnv) :- update('int', Identifier, Integer_val, Env, Env1), boolean_eval(Bool_expr, Env1, Env2, true),
+    												unaryexpr_eval(Unary_expr, Env2, Env3), general_block(Gen_block, Env3, Env4), lookup(Identifier, NewVal),
+    												for_loop_eval(t_for_loop(Identifier, NewVal, Bool_expr, Unary_expr, Gen_block), Env4, NewEnv).
+
+for_in_range_loop_eval(t_for_in_range_loop(Identifier, Integer_val1, Integer_val2, Gen_block), Env, NewEnv) :- Integer_val1 =< Integer_val2, update('int', Identifier, Integer_val1, Env, Env1), general_block(Gen_block, Env1, Env2),
+    																									NewVal is Integer_val1+1, for_in_range_loop_eval(t_for_in_range_loop(Identifier, NewVal, Integer_val2, Gen_block),Env2, NewEnv).
+for_in_range_loop_eval(t_for_in_range_loop(_, Integer_val1, Integer_val2, _), Env, Env) :- Integer_val1 > Integer_val2.
+
+print_statement_eval(t_print_statement(Identifier), Env, Env) :- lookup(Identifier, Value), write(Value), nl.
+
+declare_in_block_eval(t_decl_in_block(Identifier, Expr), Env, NewEnv) :- update(Identifier, Expr, Env, NewEnv).
+
+
 %Declaring the identifiers with values
 update_data(int, ID, Value, [], [(int, ID, Value)]) :- integer(Value).
 update_data(int, _, Value, [], []) :- not(integer(Value)), illegal_type(Value, int).
