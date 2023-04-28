6402747176508070895973b21a0294c29d6f3e3d
poornimasathyakeerthi <pskeerth@asu.edu>
2023-04-28 12:02:31 -0700

    Modified parser | Fixed errors

--- src/parser.pl
+++ src/parser.pl
@@ -1,96 +1,100 @@
+:- table identifier/3.
+:- use_rendering(svgtree).
+
 program(t_program(P)) --> block(P), [.].
+block(t_block(Decl_block, Cmd_block)) --> ['main'], ['('], [')'], ['{'], decl_block(Decl_block), command(Cmd_block), ['}'].
 
-block(t_block(decl_block,gen_block)) --> ['main'], ['('], [')'], ['{'], decl_block(decl_block), general_block(gen_block), ['}'].
-decl_block(t_decl_block(decl_block)) --> decl(decl_block), [';'].
+decl_block(Decl_block) --> decl(Decl_block).
+decl_block(t_decl_block(Decl_block, Rem_block)) --> decl(Decl_block), decl_block(Rem_block).
 
-general_block(t_gen_block(cmd_block)) --> ['{'], command(cmd_block), ['}'].
+general_block(t_gen_block(Cmd_block)) --> ['{'], command(Cmd_block), ['}'].
 
 % Declarations (initialization).
-decl(t_decl('int', identifier, integer_val, decl_block)) -->  ['int'] ,identifier(identifier) ,[':='] ,integer(integer_val), [';'], decl(decl_block).
-decl(t_decl('float', identifier, float_val, decl_block)) -->  ['float'] ,identifier(identifier) ,[':='] ,float(float_val), [';'], decl(decl_block).
-decl(t_decl('string', identifier, string_val, decl_block)) -->  ['string'] ,identifier(identifier) ,[':='] ,string(string_val), [';'], decl(decl_block).
-decl(t_decl('boolean', identifier, boolean_val, decl_block)) -->  ['boolean'] ,identifier(identifier) ,[':='] ,boolean(boolean_val), [';'], decl(decl_block).
-decl(t_decl('int', identifier)) -->  ['int'], identifier(identifier).
-decl(t_decl('float', identifier)) -->  ['float'], identifier(identifier).
+decl(t_decl('int', Identifier, Integer_val)) -->  ['int'] ,identifier(Identifier) ,[':='] ,integer(Integer_val), [';'].
+decl(t_decl('float', Identifier, Float_val)) -->  ['float'] ,identifier(Identifier) ,[':='] ,float(Float_val), [';'].
+decl(t_decl('string', Identifier, String_val)) -->  ['string'] ,identifier(Identifier) ,[':='] ,string(String_val), [';'].
+decl(t_decl('boolean', Identifier, Boolean_val)) -->  ['boolean'] ,identifier(Identifier) ,[':='] ,boolean(Boolean_val), [';'].
+
+decl(t_decl('int', Identifier)) -->  ['int'], identifier(Identifier), [';'].
+decl(t_decl('float', Identifier)) -->  ['float'], identifier(Identifier), [';'].
+
 
 %Command
-command(t_command(cmd_block, cmd)) --> commandblock(cmd_block), command(cmd).
-command(t_command(cmd_block)) --> commandblock(cmd_block).
+command(t_command(Cmd_block)) --> commandblock(Cmd_block).
+command(t_command(Cmd_block, Cmd)) --> commandblock(Cmd_block), command(Cmd).
 
 % Command block (loops, assignment, print commands).
-if_statement(t_if_statement(bool_expr, gen_block)) --> ['if'], ['('], boolean(bool_expr),  [')'], general_block(gen_block).
-if_else_statement(t_if_else_statement(bool_expr, gen_block1, gen_block2)) --> ['if'], ['('], boolean(bool_expr),  [')'], general_block(gen_block1), ['else'], general_block(gen_block2).
-while_statement(t_while_statement(bool_expr, gen_block)) --> ['while'], ['('], boolean(bool_expr),  [')'], general_block(gen_block).
-for_loop(t_for_loop(identifier, integer_val, bool_expr, unary_expr, gen_block)) --> ['for'], ['('], ['int'] ,identifier(identifier) ,['='] ,integer(integer_val), [';'], boolean(bool_expr), [';'], unaryexpr(unary_expr), [')'], general_block(gen_block).
-for_in_range_loop(t_for_in_range_loop) --> ['for'], identifier(identifier), ['in'], ['range'], ['('], integer(integer_val), [','], integer(integer_val), [')'], general_block(gen_block).
-print_statement(t_print_statement(identifier)) --> ['print'], ['('], identifier(identifier), [')'], [';'].
-declare_in_block(t_decl_in_block(identifier, expr)) --> identifier(identifier), [':='], expr(expr), [';'].
+if_statement(t_if_statement(Bool_expr, Gen_block)) --> ['if'], ['('], boolean(Bool_expr),  [')'], general_block(Gen_block).
+if_else_statement(t_if_else_statement(Bool_expr, Gen_block1, Gen_block2)) --> ['if'], ['('], boolean(Bool_expr),  [')'], general_block(Gen_block1), ['else'], general_block(Gen_block2).
+while_statement(t_while_statement(Bool_expr, Gen_block)) --> ['while'], ['('], boolean(Bool_expr),  [')'], general_block(Gen_block).
+for_loop(t_for_loop(Identifier, Integer_val, Bool_expr, Unary_expr, Gen_block)) --> ['for'], ['('], ['int'] ,identifier(Identifier) ,[':='] ,integer(Integer_val), [';'], boolean(Bool_expr), [';'], unaryexpr(Unary_expr), [')'], general_block(Gen_block).
+for_in_range_loop(t_for_in_range_loop(Identifier, Integer_val1, Integer_val2, Gen_block)) --> ['for'], identifier(Identifier), ['in'], ['range'], ['('], integer(Integer_val1), [','], integer(Integer_val2), [')'], general_block(Gen_block).
+print_statement(t_print_statement(Identifier)) --> ['print'], ['('], identifier(Identifier), [')'], [';'].
+declare_in_block(t_decl_in_block(Identifier, Expr)) --> identifier(Identifier), [':='], expr(Expr), [';'].
 
-commandblock(t_command_block(statement)) --> if_statement(statement) | if_else_statement(statement) | while_statement(statement) | for_loop(statement) |
-                 for_in_range_loop(statement) | print_statement(statement) | declare_in_block(statement) | general_block(statement).
-commandblock(t_command_block(statement, cmd)) --> if_statement(statement), command(cmd) | if_else_statement(statement), command(cmd) | while_statement(statement), command(cmd) |
-                 for_loop(statement), command(cmd) | for_in_range_loop(statement), command(cmd) | print_statement(statement), command(cmd) |
-                 declare_in_block(statement), command(cmd) | general_block(statement), command(cmd).
+commandblock(t_command_block(Statement)) --> if_statement(Statement) | if_else_statement(Statement) | while_statement(Statement) | for_loop(Statement) |
+                 for_in_range_loop(Statement) | print_statement(Statement) | declare_in_block(Statement) | general_block(Statement).
+commandblock(t_command_block(Statement, Cmd)) --> if_statement(Statement), command(Cmd) | if_else_statement(Statement), command(Cmd) | while_statement(Statement), command(Cmd) |
+                 for_loop(Statement), command(Cmd) | for_in_range_loop(Statement), command(Cmd) | print_statement(Statement), command(Cmd) |
+                 declare_in_block(Statement), command(Cmd) | general_block(Statement), command(Cmd).
 
 %Increment decrement operators
-unaryexpr(t_unary_expr(identifier, '+', '+')) --> identifier(identifier), ['+'], ['+'].
-unaryexpr(t_unary_expr(identifier, '-', '-')) --> identifier(identifier), ['-'], ['-'].
+unaryexpr(t_unary_expr(Identifier, '+', '+')) --> identifier(Identifier), ['+'], ['+'].
+unaryexpr(t_unary_expr(Identifier, '-', '-')) --> identifier(Identifier), ['-'], ['-'].
 
-ternaryexpr(t_ternary_expr(bool_expr, expr1, expr2)) --> ['('], boolean(bool_expr), [')'], ['?'], expr(expr1), [':'], expr(expr2).
+ternaryexpr(t_ternary_expr(Bool_expr, Expr1, Expr2)) --> ['('], boolean(Bool_expr), [')'], ['?'], expr(Expr1), [':'], expr(Expr2).
 
 % Expressions
-expr(t_expr(factor)) --> factor(factor).
-expr(t_expr(mini_expr)) --> miniexpr(mini_expr).
-expr(t_expr(left_mini_expr, '+', right_expr)) --> miniexpr(left_mini_expr), ['+'], expr(right_expr).
-expr(t_expr(factor, '+', mini_expr)) --> factor(factor), ['+'], miniexpr(mini_expr).
-expr(t_expr(left_mini_expr, '-', right_expr)) --> miniexpr(left_mini_expr), ['-'], expr(right_expr).
-expr(t_expr(factor, '-', mini_expr)) --> factor(factor), ['-'], miniexpr(mini_expr).
-expr(t_expr(ternary_expr)) --> ternaryexpr(ternary_expr).
+expr(t_expr(Factor)) --> factor(Factor).
+expr(t_expr(Mini_expr)) --> miniexpr(Mini_expr).
+expr(t_expr(Left_mini_expr, '+', Right_expr)) --> miniexpr(Left_mini_expr), ['+'], expr(Right_expr).
+expr(t_expr(Factor, '+', Mini_expr)) --> factor(Factor), ['+'], miniexpr(Mini_expr).
+expr(t_expr(Left_mini_expr, '-', Right_expr)) --> miniexpr(Left_mini_expr), ['-'], expr(Right_expr).
+expr(t_expr(Factor, '-', Mini_expr)) --> factor(Factor), ['-'], miniexpr(Mini_expr).
+expr(t_expr(Ternary_expr)) --> ternaryexpr(Ternary_expr).
 
 % To incorporate precedence rules.
-miniexpr(t_miniexpr(factor)) --> factor(factor).
-miniexpr(t_miniexpr(factor, '*', mini_expr)) --> factor(factor), ['*'], miniexpr(mini_expr).
-miniexpr(t_miniexpr(factor, '/', mini_expr)) --> factor(factor), ['/'], miniexpr(mini_expr).
+miniexpr(t_miniexpr(Factor)) --> factor(Factor).
+miniexpr(t_miniexpr(Factor, '', Mini_expr)) --> factor(Factor), [''], miniexpr(Mini_expr).
+miniexpr(t_miniexpr(Factor, '/', Mini_expr)) --> factor(Factor), ['/'], miniexpr(Mini_expr).
 
 % Lowest factor in th expressions.
-factor(t_factor(identifier)) --> identifier(identifier).
-factor(t_factor(num)) --> number(num).
+factor(t_factor(Identifier)) --> identifier(Identifier).
+factor(t_factor(Num)) --> number(Num).
 
 % Boolean expressions.
-boolean(t_boolean(bool_expr)) --> ['true'] | ['false'] | ['not'], boolean(bool_expr).
-boolean(t_boolean(expr1, expr2, bool_expr)) --> expr(expr1), ['='], expr(expr2) , ['and'], boolean(bool_expr) | expr(expr1), ['='], expr(expr2), ['or'], boolean(bool_expr).
-boolean(t_boolean(expr1, expr2)) --> expr(expr1), ['='], expr(expr2).
+boolean(t_boolean(Bool_expr)) --> ['true'] | ['false'] | ['not'], boolean(Bool_expr).
+boolean(t_boolean(Expr1, Expr2, bool_expr)) --> expr(Expr1), ['='], expr(Expr2) , ['and'], boolean(bool_expr) | expr(Expr1), ['='], expr(Expr2), ['or'], boolean(bool_expr).
+boolean(t_boolean(Expr1, Expr2)) --> expr(Expr1), ['='], expr(Expr2) |  expr(Expr1), ['>'], expr(Expr2) |
+    								 expr(Expr1), ['<'], expr(Expr2) |  expr(Expr1), ['>='], expr(Expr2) |
+    								 expr(Expr1), ['=<'], expr(Expr2) |  expr(Expr1), ['!='], expr(Expr2).
 
 % identifier variables.
-identifier(t_identifier(lowercase_let, identifier_tail)) --> lowercase_letters(lowercase_let), identifier_tail(identifier_tail).
-identifier_tail(t_identifier_tail(uppercase_let, identifier_tail)) --> uppercase_letters(uppercase_let), identifier_tail(identifier_tail).
-identifier_tail(t_identifier_tail(identifier_tail)) --> ['_'], identifier_tail(identifier_tail).
-identifier_tail('').
-
+%identifier(t_identifier(Lowercase_let)) --> lowercase_letters(Lowercase_let).
+identifier(t_identifier([Lowercase_let|Identifier_tail])) --> lowercase_letters(Lowercase_let), identifier_tail(Identifier_tail).
+identifier_tail(t_identifier_tail(Uppercase_let, Identifier_tail)) --> uppercase_letters(Uppercase_let), identifier_tail(Identifier_tail).
+identifier_tail(t_identifier_tail(Identifier_tail)) --> ['_'], identifier_tail(Identifier_tail).
+identifier_tail(_) --> [].
 
 % Strings
-string(t_string(lowercase, string_val)) --> lowercase_letters(lowercase), string(string_val).
-string(t_string(uppercase, string_val)) --> uppercase_letters(uppercase), string(string_val).
-string(t_string(num, string_val)) --> number(num), string(string_val).
-string(t_string(lowercase)) --> lowercase_letters(lowercase).
-string(t_string(uppercase)) --> uppercase_letters(uppercase).
-string(t_string(num)) --> number(num).
-
+string(t_string(String)) --> string_val(String).
 % Numbers
-integer(t_integer(num)) --> number(num).
-integer(t_integer(num, integer_val)) --> number(num), integer(integer_val).
-float(t_float(int_part, frac_part)) --> integer(int_part), ['.'], integer(frac_part).
+integer(t_integer(Num)) --> integer_val(Num).
+float(t_float(Float)) --> float_val(Float).
+
+integer_val(V, [V | T], T) :- atom_number(V, V1), integer(V1).
+float_val(V, [V | T], T) :- atom_number(V, V1), float(V1).
+string_val(V, [V | T], T) :- string(V).
 
 % String part.
-lowercase_letters(t_lowercase_letters) --> ['a'] | ['b'] | ['c'] | ['d'] | ['e'] | ['f'] |
+lowercase_letters(t_lc) --> ['a'] | ['b'] | ['c'] | ['d'] | ['e'] | ['f'] |
     ['g'] | ['h'] | ['i'] | ['j'] | ['k'] | ['l'] | ['m'] | ['n'] | ['o'] |
     ['p'] | ['q'] | ['r'] | ['s'] | ['t'] | ['u'] | ['v'] | ['w'] | ['x'] |
     ['x'] | ['y'] | ['z'].
-uppercase_letters(t_uppercase_letters) --> ['A'] | ['B'] | ['C'] | ['D'] | ['E'] | ['F'] |
+uppercase_letters(t_uc) --> ['A'] | ['B'] | ['C'] | ['D'] | ['E'] | ['F'] |
     ['G'] | ['H'] | ['I'] | ['J'] | ['K'] | ['L'] | ['M'] | ['N'] | ['O'] |
     ['P'] | ['Q'] | ['R'] | ['S'] | ['T'] | ['U'] | ['V'] | ['W'] | ['X'] |
     ['X'] | ['Y'] | ['Z'].
 
 % Nums
 number(t_number) --> ['0'] | ['1'] | ['2'] | ['3'] | ['4'] | ['5'] | ['6'] | ['7'] | ['8'] | ['9'].
-
